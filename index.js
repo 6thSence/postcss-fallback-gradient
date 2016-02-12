ba9bed2e4928fa6552fbb71a9a3db8977cf7a924
6thSence <d.pushkarskaya.pr@gmail.com>
2016-02-12 16:31:04 +0600

    сортировка и расстановка процентов. Закончена формулировка rbga модели цвета

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const postcss = require ('postcss');
-const hexToRgb, srtToArray, getPercent, createColorModel = require('./utils');
+const utils = require('./utils');
 
 module.exports = postcss.plugin('myplug', function myplug(options) {
     return (css) => {
         options = options || {};
-            
-            var pravda = true;
 
         css.walkRules(function (rule) {
             var firstRull = true;
@@ -14,15 +12,24 @@ module.exports = postcss.plugin('myplug', function myplug(options) {
             rule.walkDecls('background', function (decl) {
 
                 if (decl.value.indexOf('gradient') !== -1) {
-                    colorModel = createColorModel(decl, firstRull, colorModel);
+                    if (firstRull) {
+                        colorModel = utils.createColorModel(decl, firstRull, colorModel); // построение модели цветов
+                        colorModel = utils.checkOfTransparent(colorModel); // обратока transparent
+                        colorModel = utils.sortByPercent(colorModel); // сортировка без смешения undefined процентов
+                        colorModel = utils.checkOfPercent(colorModel); // расстановка процентов там где их нет 
+                        colorModel = utils.sortByPercent(colorModel); // повторная сортировка с новыми процентами
+                        console.log(colorModel);
+                        console.log('\n');
+                    }
                 }
-                
 
                 if (firstRull) {
                     firstRull = false;
-                    console.log(colorModel);
-                    // const middleColor = createMiddleColor(); //TODO
+                    // console.log(colorModel);
+
+                    // TODO:
                     // 
+                    // const middleColor = createMiddleColor();
                     // 
                     // return rule.insertBefore(decl, {
                     //     prop: 'background'
--- utils.js
+++ utils.js
@@ -1,16 +1,34 @@
 const colorTable = require ('./crayola.json');
 
+
+const sortByPercent = (colorModel) => {
+    colorModel.sort((a, b) => {
+        if (a.percent > b.percent) {
+            return 1;
+        } else if (a.percent < b.percent) {
+            return -1;
+        }
+
+        return 0;
+    });
+
+    return colorModel;
+};
+
 const hexToRgb = (hex) => {
+
     if (hex.length ===4 ) {
         hex = '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
-    };
+    }
+
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
     return result ? {
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
-}
+};
 
 const srtToArray = (str) => {
     var array = [];
@@ -21,9 +39,11 @@ const srtToArray = (str) => {
     while (pos !== len) {
         pos++;
         if (str[pos]===' ' || str[pos]===',' || str[pos]==='(' || str[pos]===')' || pos===len) {
+
             if (arg!=='') {
                 array.push(arg);
-            };
+            }
+
             arg='';
         }else {
             arg = arg + str[pos];
@@ -31,18 +51,94 @@ const srtToArray = (str) => {
     } 
 
     return array;
-}
+};
 
 const getPercent = (pos, array) => {
     var percent = 0;
+
     if (array[pos] && array[pos].indexOf('%') !=-1) {
-        percent = array[pos].slice(0, array[pos].indexOf('%'));
-    } else if (!array[pos]  && pos === array.length){
+
+        percent = + array[pos].slice(0, array[pos].indexOf('%'));
+
+    } else if (!array[pos] && pos === array.length){
+
         percent = 100;
-    };
+    }
 
     return percent;
-}
+};
+
+const checkOfTransparent = (colorModel) => {
+
+    colorModel.forEach(function(item, i, arr) {
+        if (item.r === 'transparent') {
+            switch (item.g) {
+            case '1':
+                item.r=arr[i-1].r;
+                item.g=arr[i-1].g;
+                item.b=arr[i-1].b;
+                item.a=0;
+                item.percent=undefined;
+                break;
+            case '2':
+                item.r=arr[i+1].r;
+                item.g=arr[i+1].g;
+                item.b=arr[i+1].b;
+                item.a=0;
+                item.percent=undefined;
+                break;
+            default:
+                break;
+            }
+        }
+    });
+
+    return colorModel;
+};
+
+const checkOfPercent = (colorModel) => {
+
+    var und = 0; // колличество undefinde 
+    var min = 0; // значение до undefinde
+    var max = 0; // значение после undefinde
+    var coef = 0; // разница между undefinde елементами
+    var val = 0; // значение присваемое елементу 
+    var pos = -1; // позиция map-a
+
+    colorModel.map( item => {
+        pos = pos + 1;
+
+        if (item.percent === undefined) {
+
+            und = und + 1;
+
+        } else if (und > 0) {
+
+            min = max;
+            max = item.percent;
+            coef = (max + min) / und;
+            val = max - coef;
+
+            while (und > 0) {
+                colorModel[pos - und].percent = coef;
+                val = val + coef;
+                und = und -  1;
+            }
+
+            min = max;
+            max = 0;
+            und = 0;
+
+        } else if (item.percent > max) {
+
+            max = item.percent;
+
+        }
+
+    });
+
+    return colorModel;
+};
 
 const createColorModel = (decl, firstRull, colorModel) => {
     const val = decl.value.slice(decl.value.indexOf(',')+1,decl.value.length-1);
@@ -56,64 +152,69 @@ const createColorModel = (decl, firstRull, colorModel) => {
         if ( array[pos] && (array[pos].indexOf('#') === 0) ) { // Если цвет hex
             var color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
             var rgb = hexToRgb(array[pos].toUpperCase());
-            color.r = rgb.r; 
-            color.g = rgb.g; 
-            color.b = rgb.b; 
+            color.r = +rgb.r; 
+            color.g = +rgb.g; 
+            color.b = +rgb.b; 
             color.a = 1;
-            color.percent = getPercent(pos+1, array)
+            color.percent = getPercent(pos+1, array);
 
-            if (firstRull) {
-                colorModel.push(color);
-            }
+            colorModel.push(color);
+
         } else {
             if (array[pos] && !array[pos].match(/\d+/) ) { // Если цвет rgbs
                 if (array[pos]==='rgba') { 
-                    var color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
-                    color.r = array[pos+1];
-                    color.g = array[pos+2];
-                    color.b = array[pos+3];
-                    color.a = array[pos+4];
+                    color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
+                    color.r = +array[pos+1];
+                    color.g = +array[pos+2];
+                    color.b = +array[pos+3];
+                    color.a = +array[pos+4];
                     color.percent = getPercent(pos+5, array);
 
-                    if (firstRull) {
-                        colorModel.push(color);
-                    }
+                    colorModel.push(color);
+
                 } else if (array[pos]==='rgb') {  // Если цвет rgb 
-                    var color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
-                    color.r = array[pos+1];
-                    color.g = array[pos+2];
-                    color.b = array[pos+3];
+                    color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
+                    color.r = +array[pos+1];
+                    color.g = +array[pos+2];
+                    color.b = +array[pos+3];
                     color.a = 1;
                     color.percent = getPercent(pos+4, array);
 
-                    if (firstRull) {
-                        colorModel.push(color);
-                    }
+                    colorModel.push(color);
+
                 } else { // Если цвет написан словом
+
+                    if (array[pos] === 'transparent') { // Если это transparent
+                        var color1 = { 'r': 'transparent', 'g': '1', 'b': '', 'a': '', 'percent': '' };
+                        var color2 = { 'r': 'transparent', 'g': '2', 'b': '', 'a': '', 'percent': '' };
+
+                        colorModel.push(color1);
+                        colorModel.push(color2);
+                    }
+
                     for (item in colorTable) {
-                        if (item === array[pos]) {
-                            var color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
-                            var rgb = hexToRgb(colorTable[item].toUpperCase());
-                            color.r = rgb.r; 
-                            color.g = rgb.g; 
-                            color.b = rgb.b; 
+
+                        if (item === array[pos]) { // Если цвет из таблицы цветов
+                            color = { 'r': '', 'g': '', 'b': '', 'a': '', 'percent': '' };
+                            rgb = hexToRgb(colorTable[item].toUpperCase());
+                            color.r = +rgb.r; 
+                            color.g = +rgb.g; 
+                            color.b = +rgb.b; 
                             color.a = 1;
                             color.percent = getPercent(pos+1, array);
                             
-                            if (firstRull) { // Если первое правило
-                                colorModel.push(color);
-                            }
+                            colorModel.push(color);
                         };
-                    } 
+
+                    };
                 }
             }   
         }
     }
-
     return colorModel;
-}
+};
 
-module.exports = hexToRgb;
-module.exports = srtToArray;
-module.exports = getPercent;
-module.exports = createColorModel;
+module.exports.createColorModel = createColorModel;
+module.exports.checkOfTransparent = checkOfTransparent;
+module.exports.checkOfPercent = checkOfPercent;
+module.exports.sortByPercent = sortByPercent;
