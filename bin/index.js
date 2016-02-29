c0de7d1646f394f982c33e38c77e113fbb49d472
6thSence <d.pushkarskaya.pr@gmail.com>
2016-02-29 20:09:24 +0600

    add compose

--- bin/index.js
+++ bin/index.js
@@ -4,15 +4,6 @@ var _utils = require('./utils');
 
 var postcss = require('postcss');
 
-//
-//const createColor = compose(
-//    getMiddleColor,
-//    getTwoMaxColors,
-//    sortByPercent,
-//    checkOfPercent,
-//    sortByPercent,
-//    checkOfTransparent,
-//    createColorModel);
 
 module.exports = postcss.plugin('myplug', function myplug(options) {
     return function (css) {
@@ -28,6 +19,7 @@ module.exports = postcss.plugin('myplug', function myplug(options) {
                 if (decl.value.indexOf('gradient') !== -1) {
                     if (firstRull) {
                         var middleColor = (0, _utils.compose)(_utils.getMiddleColor, _utils.getTwoMaxColors, _utils.sortByPercent, _utils.checkOfPercent, _utils.sortByPercent, _utils.checkOfTransparent, _utils.createColorModel)(decl, firstRull);
+
                         colorRgba = 'rgba(' + middleColor.r + ', ' + middleColor.g + ', ' + middleColor.b + ', ' + middleColor.a + ')';
                         colorHex = (0, _utils.rbgToHex)(middleColor); // получаем hex цвет из rgba
                     }
